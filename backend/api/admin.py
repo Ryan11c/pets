a1a950fed591563fef1658cd5b1ed523ac7859ba
Ryan Choi <ryan04122005@gmail.com>
2025-04-24 00:36:29 -0700

    Set up API for pets and configure settings for images

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Pet
 
 # Register your models here.
+
+#Register Pet model to admin panel
+admin.site.register(Pet)
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+#Our pet model
+class Pet(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.CharField(max_length=100, blank=True) 
+    date = models.DateField(auto_now=True)
+    picture = models.ImageField(null=True, blank=True, upload_to='images/')
+    def __str__(self):
+        return f"Name: {self.name}"
+    
